fa03a39ac9d86b4d9ea4bf1832137bc39d8b15e7
Devilfish281 <Dobley@csu.fullerton.edu>
2025-03-27 08:52:39 -0700

    Added docker to asset process service ai

--- asset-processing-service-ai/asset_processing_service_ai/main.py
+++ asset-processing-service-ai/asset_processing_service_ai/main.py
@@ -1,5 +1,13 @@
+from time import sleep
+
+
 def main():
     print("Hello, world!")
+    i = 1  # initialize the counter
+    while i <= 10:
+        print(f"[{i}] Hello, world!", flush=True)
+        i += 1
+        sleep(1)
 
 
 if __name__ == "__main__":
