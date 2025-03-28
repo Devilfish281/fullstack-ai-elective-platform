504782425457bf3f5cd2bb1b533ce125b10d1fde
Devilfish281 <Dobley@csu.fullerton.edu>
2025-03-28 16:39:51 -0700

    stubbed out all landing and dashboard routes async3

--- nextjs/app/(dashboard)/project/[projectId]/page.tsx
+++ nextjs/app/(dashboard)/project/[projectId]/page.tsx
@@ -2,13 +2,11 @@ import { notFound } from "next/navigation";
 import React from "react";
 
 interface ProjectPageProps {
-  params: {
-    projectId: string;
-  };
+  params: Promise<{ projectId: string }>; // Changed Code
 }
 
 export default async function ProjectPage({ params }: ProjectPageProps) {
-  const { projectId } = await Promise.resolve(params);
-  if (projectId !== "123") return notFound();
+  const { projectId } = await params; // Changed Code
+  if (projectId !== "123") return notFound(); // Changed Code
   return <div>ProjectPage: {projectId} </div>;
 }
--- nextjs/app/(dashboard)/template/[templateId]/page.tsx
+++ nextjs/app/(dashboard)/template/[templateId]/page.tsx
@@ -2,13 +2,11 @@ import { notFound } from "next/navigation";
 import React from "react";
 
 interface TemplatePageProps {
-  params: {
-    templateId: string;
-  };
+  params: Promise<{ templateId: string }>; // Changed Code
 }
 
 export default async function TemplatePage({ params }: TemplatePageProps) {
-  const { templateId } = await Promise.resolve(params);
-  if (templateId !== "123") return notFound();
+  const { templateId } = await params; // Changed Code
+  if (templateId !== "123") return notFound(); // Changed Code
   return <div>TemplatePage: {templateId} </div>;
 }
